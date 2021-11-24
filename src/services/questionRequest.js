d070db603d9071fb40f4fe3720132417fc7be0ff
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-24 20:07:43 -0300

    Adiciona requisição para as questions 5 de cada vez

--- src/services/questionRequest.js
+++ src/services/questionRequest.js
@@ -1,14 +1,11 @@
 const questionsRequest = async (token) => {
   try {
     const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
-    const json = await response.json();
-    console.log(json);
-    return response.ok ? Promise.resolve(json) : Promise.reject(json);
+    const { results } = await response.json();
+    return response.ok ? Promise.resolve(results) : Promise.reject(results);
   } catch (err) {
     console.log(err);
   }
 };
-
-questionsRequest();
 
 export default questionsRequest;
