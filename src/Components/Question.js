27f97be9074f15d2af6ca0dced7cafb2489e82fc
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-24 20:09:35 -0300

    Adiciona parte inicial do componente question

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,7 +1,17 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import questionsRequest from '../services/questionRequest';
 
 class Question extends Component {
+  async awaitfetch() {
+    const { token } = this.props;
+    const teste = await questionsRequest(token);
+    console.log(teste, 'categories');
+  }
+
   render() {
+    this.awaitfetch();
     return (
       <div>
         <h2>{ }</h2>
@@ -10,4 +20,12 @@ class Question extends Component {
   }
 }
 
-export default Question;
+const mapStateToProps = (state) => ({
+  token: state.token.state,
+});
+
+export default connect(mapStateToProps)(Question);
+
+Question.propTypes = {
+  token: PropTypes.string.isRequired,
+};
--- src/Pages/GameScreen.jsx
+++ src/Pages/GameScreen.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import Question from '../Components/Question';
 
 class GameScreen extends React.Component {
   render() {
     return (
-      <header>
-        <image
-          data-testid="header-profile-picture"
-        />
-        <p
-          data-testid="header-player-name"
-        >
-          Nome
-        </p>
-        <p
-          data-testid="header-score"
-        >
-          0
-        </p>
-      </header>
+      <>
+        <header>
+          <image
+            data-testid="header-profile-picture"
+          />
+          <p
+            data-testid="header-player-name"
+          >
+            Nome
+          </p>
+          <p
+            data-testid="header-score"
+          >
+            0
+          </p>
+        </header>
+        <Question />
+      </>
     );
   }
 }
