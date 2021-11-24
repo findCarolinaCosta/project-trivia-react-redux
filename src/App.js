86838505622446adfa42e3f8cc26d0b38c6f2ca9
viniciusfernands <viniciusfernandes126@gmail.com>
2021-11-24 18:23:58 -0300

    [Projeto] Realiza correção no lint do projeto

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
       <Route
         exact
         path="/"
-        render={ (props) => <Login { ...props } /> }
+        component={ Login }
       />
       <Route
         path="/game"
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPlayer } from '../actions';
 
