52296b71ea150cb851d1743cb58df8f53785de21
viniciusfernands <viniciusfernandes126@gmail.com>
2021-11-25 15:52:00 -0300

    [Requisito 4] Refatora parte do código

--- src/Pages/GameScreen.jsx
+++ src/Pages/GameScreen.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import convertEmailToHash from '../services/gravatarRequest';
 
 class GameScreen extends React.Component {
-  render() {
-    const { name, email } = this.props;
-    const link = convertEmailToHash(email);
+  constructor() {
+    super();
+    this.catchPicture = this.catchPicture.bind(this);
+  }
 
+  catchPicture() {
+    const { ranking, name } = this.props;
+    const link = ranking.find((data) => data.name === name);
+    return link.picture;
+  }
+
+  render() {
+    const { name } = this.props;
     return (
       <header>
         <img
           data-testid="header-profile-picture"
           alt="Foto do perfil"
-          src={ link }
+          src={ this.catchPicture() }
         />
         <p
           data-testid="header-player-name"
@@ -32,12 +40,12 @@ class GameScreen extends React.Component {
 
 GameScreen.propTypes = {
   name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
+  ranking: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
 
 const mapStateToProps = (state) => ({
   name: state.player.name,
-  email: state.player.email,
+  ranking: state.ranking,
 });
 
 export default connect(mapStateToProps)(GameScreen);
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import tokenRequest from '../services/tokenRequest';
 
 export const GET_PLAYER_TOKEN = 'GET_PLAYER_TOKEN';
+export const GET_USER = 'GET-USER';
+export const GET_USER_RANKING = 'GET_USER_RANKING';
 
 export const getPlayerToken = (token) => ({
   type: GET_PLAYER_TOKEN,
@@ -16,6 +18,12 @@ export const getPlayer = () => async (dispatch) => {
   }
 };
 
-export const GET_USER = 'GET-USER';
+export const getUser = (data) => ({
+  type: GET_USER,
+  data,
+});
 
-export const getUser = (data) => ({ type: GET_USER, data });
+export const getUserRanking = (payload) => ({
+  type: GET_USER_RANKING,
+  payload,
+});
