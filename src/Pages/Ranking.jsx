ea6db0f85b1244f283253f1716566d66f22eaf0a
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-11-30 15:10:12 -0300

    Requisito 18

--- src/Pages/Ranking.jsx
+++ src/Pages/Ranking.jsx
@@ -1,13 +1,37 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.onClick }
+        >
+          Voltar ao Início
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
