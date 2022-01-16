0302c7c310fa49227f840f5b0047d3069f93d9b3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-16 16:07:55 -0300

    Updates

---
+++ precache-manifest.6deb140f91109b2c56006de8448fcbef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2155bbddd7535c187eee86211382eb9c",
+    "url": "/project-trivia-react-redux/index.html"
+  },
+  {
+    "revision": "246904ac00c5eec0ced5",
+    "url": "/project-trivia-react-redux/static/css/main.2b46d64a.chunk.css"
+  },
+  {
+    "revision": "2edb183cba0119cecc96",
+    "url": "/project-trivia-react-redux/static/js/2.0ea0d307.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/project-trivia-react-redux/static/js/2.0ea0d307.chunk.js.LICENSE"
+  },
+  {
+    "revision": "246904ac00c5eec0ced5",
+    "url": "/project-trivia-react-redux/static/js/main.3f6ccbf8.chunk.js"
+  },
+  {
+    "revision": "5f1245b44c44d3da06a6",
+    "url": "/project-trivia-react-redux/static/js/runtime-main.2ea505b7.js"
+  }
+]);
